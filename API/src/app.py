b7b6b109477d71de5434987d10370fafbd957ab6
MyticFrez07 <andresfelipe2816@gmail.com>
2023-03-12 21:29:16 -0500

    primer avance

--- API/src/app.py
+++ API/src/app.py
@@ -1,6 +1,6 @@
 
-from src.Rutas.UsuarioCRUD import UsuarioCRUD
-from src.database import app
+from API.src.Rutas.UsuarioCRUD import UsuarioCRUD
+from API.src.database import app
 
 
 app.register_blueprint(UsuarioCRUD)
--- API/src/database.py
+++ API/src/database.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://NombreUsuario:NombreUsuario@localhost/NombreBaseDatos'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:F3lipe2816.@localhost/Api'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
