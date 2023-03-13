1440da645332d8dab7a9848df932fb672dededac
Juan Mendoza <juandavidmendozavargas123@gmail.com>
2023-03-12 20:37:54 -0500

    CRUD de roles

--- API/src/app.py
+++ API/src/app.py
@@ -1,17 +1,10 @@
-from flask import Flask
-from src.Rutas.UsuarioCRUD import UsuarioCRUD
-from src.database import db
+from API.src.Rutas.UsuarioCRUD import UsuarioCRUD
+from API.src.Rutas.RolCRUD import RolCRUD
+from API.src.database import app
 
-app_principal = Flask(__name__)
-app_principal.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Juancarrillo09@localhost/ServicioWeb'
-app_principal.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app_principal)
-
-with app_principal.app_context():
-    db.create_all()
-
-app_principal.register_blueprint(UsuarioCRUD)
+app.register_blueprint(UsuarioCRUD)
+app.register_blueprint(RolCRUD)
 
 if __name__ == '__main__':
-    app_principal.run(debug=True)
+    app.run(debug=True)
--- API/src/database.py
+++ API/src/database.py
@@ -2,5 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Juancarrillo09@localhost/ServicioWeb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:bdpostgre1608@localhost/api'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
