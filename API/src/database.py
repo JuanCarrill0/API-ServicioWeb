dd965f54dbcfce44142cd485138ebeab31665089
Juan Esteban Carrillo Garcia <ju4nc4rrill00@gmail.com>
2023-03-13 17:13:26 -0500

    Actualizacion Merge

--- API/src/database.py
+++ API/src/database.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://NombreUsuario:Contraseña@localhost/NombreBaseDatos'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Juancarrillo09@localhost/ServicioWeb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
