5a80919a7ef6ebb7b9f69f650d8e52d822a437e8
MyticFrez07 <andresfelipe2816@gmail.com>
2023-03-13 17:07:58 -0500

    Permisos CRUD v2

--- API/src/app.py
+++ API/src/app.py
@@ -1,10 +1,13 @@
 
 from API.src.Rutas.UsuarioCRUD import UsuarioCRUD
+from API.src.Rutas.RolCRUD import RolCRUD
+from API.src.Rutas.PermisoCRUD import PermisoCRUD
 from API.src.database import app
 
 
 app.register_blueprint(UsuarioCRUD)
+app.register_blueprint(RolCRUD)
+app.register_blueprint(PermisoCRUD)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    print("hola mundo")
